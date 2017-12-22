cd1e36ae2c6d5095b72782ab07de705b44724e8f
Razorcake <sp00kybug@yahoo.ca>
2017-12-22 03:16:25 -0800

    lots of changes to work page styles. Looks a lot better, but still needs the top bit fixed. Changed banner and specialty sections.

--- wp-content/themes/fluida-child/page-work.php
+++ wp-content/themes/fluida-child/page-work.php
@@ -23,7 +23,7 @@ get_header(); ?>
 				<div class="port-intro-box">
 					<div class="port-intro">
 			<h1>Welcome!</h1>
-			<h4>I'm Natalie Rose, your web solution specialist.</h4>
+			<h4>I'm Natalie Rose,<br> your web solution specialist.</h4>
 					</div>
 				</div>
 
@@ -35,7 +35,7 @@ get_header(); ?>
 							<span
 							class="txt-rotate"
 							data-period="2000"
-							data-rotate='[ "web & graphic design!", "web development!", "custom WordPress themes!", "responsive design transitions!", "E-Commerce solutions!", "site transfers!", "website maintenance!" ]'></span>
+							data-rotate='[ "web & graphic design!", "branding & logo design!", "web development!", "custom WordPress themes!", "responsive design transitions!", "E-Commerce solutions!", "site transfers!", "website maintenance!" ]'></span>
 					</h3>
 
 				<script type="text/javascript">
@@ -105,7 +105,7 @@ get_header(); ?>
 
 				<div class="desc-hire-holder">
 			<div class="port-desc">
-					<h5>I offer individually tailored services for your web development needs.</h5>
+					<h5>I tailor all of my services to suit your individual needs.</h5>
 					<p>If you are a small business or an individual with products, services, ideas, or inspiration to share there is no better way to reach people around the world than with a website or blog.</p>
 					<p>I specialize in crafting websites for an affordable price. I work with HTML5, CSS3, PHP, WordPress, and a touch of JavaScript & JQuery to create unique custom websites. </p>
 			</div>
@@ -134,14 +134,14 @@ get_header(); ?>
 						</div>
 						<div id="dotext1">
 							<p><strong>Web & Graphic Design</strong></p>
-								<p>I design elegant & stylish websites with a modern edge. I can design your entire website from start to finish. I also do graphic design, and can design your printed materials, such as posters, brochures, and business cards.</p>
+								<p>I design elegant & stylish websites with a modern edge. I can design your entire website from start to finish. Need a set of graphics for your social media ads? I can do that for you! I also do print media and can design your printed materials, such as posters, brochures, and business cards. Need a fresh new look on your website or an updated logo? I do branding and re-design work to keep your site sleek and on trend.</p>
 						</div>
 					</div>
 
 					<div id="dobox2">
 						<div id="dotext2">
 							<p><strong>Web Development</strong></p>
-								<p>I craft, code, and customize unique websites for small businesses, individuals, and start-ups. I can code everything up from scratch, or work on your existing website easily.</p>
+								<p>I craft, code, and customize unique websites for all types of businesses. I can code everything up from scratch and build you a one-of-a-kind website from the ground up. Already have a website? I can fix bugs, update your info, or make quick functionality fixes. From completely new projects to small fixes on existing sites, I'm ready to dig in! No job is too big or small.</p>
 						</div>
 						<div id="docon2">
 							<img src="/wp-content/uploads/2017/12/051-laptop230.png" alt="development icon">
@@ -154,14 +154,14 @@ get_header(); ?>
 						</div>
 						<div id="dotext3">
 							<p><strong>Custom WordPress Themes</strong></p>
-								<p>I can build you a completely new theme from scratch or customize an existing WordPress theme, depending on your needs and budget.</p>
+								<p>Do you find the out-of-the-box WordPress themes boring and limiting? Want to add your own flair to make your website really stand out? I can build you a completely new theme from scratch or customize an existing WordPress theme that you love. With a custom WordPress child theme, the options are limitless. You will be in complete control of the content you add, while I handle the custom layout options to make your website truly unique.</p>
 						</div>
 					</div>
 
 					<div id="dobox4">
 						<div id="dotext4">
 							<p><strong>Responsive Design Transitions</strong></p>
-								<p>If your website is static or functions poorly on mobile devices, I can give it a thorough responsive overhaul to keep it current and competitive.</p>
+								<p>No one likes a website thats frustrating to use on a tablet or mobile, especially when your competitor's is easy to use. If your website is static or functions poorly on mobile devices, I can give it a thorough responsive overhaul to keep it current and competitive. Don't lose money over poor design! I have solutions!</p>
 						</div>
 						<div id="docon4">
 							<img src="/wp-content/uploads/2017/12/051-responsive230.png" alt="responsive icon">
@@ -174,14 +174,14 @@ get_header(); ?>
 						</div>
 						<div id="dotext5">
 							<p><strong>E-Commerce Solutions</strong></p>
-								<p>If you have products to sell there's no better way to reach a large audience than online. Setting up an E-Commerce website doesn't have to be daunting or unaffordable. I can install, add products, test, and set up your shipping and payment options for you so you can get those products in front of and out to the people who need them.</p>
+								<p>If you have products to sell there's no better way to reach a large audience than online. Setting up an E-Commerce website doesn't have to be daunting or unaffordable. I can install, add products, test, and set up your shipping and payment options for you so you can get those products in front of and out to the people who need them. Reach a wider audience, sell more products & make more money. The investment is worth it and you will thank yourself in the future!</p>
 						</div>
 					</div>
 
 					<div id="dobox6">
 						<div id="dotext6">
 							<p><strong>Site Transfers</strong></p>
-								<p>Transfering a current website and migrating content to another hosting platform can be time-consuming and complicated. I am here when you need website transfer help, and will take care of the entire process. </p>
+								<p>Tired of your old platform? Need something better? Transfering a current website and migrating content to another hosting platform can be time-consuming and complicated. But there's no need to struggle on your own! I am here when you need website transfer help, and will take care of the entire process for you. </p>
 						</div>
 						<div id="docon6">
 							<img src="/wp-content/uploads/2017/12/051-networking230.png" alt="transfer icon">
@@ -194,7 +194,7 @@ get_header(); ?>
 						</div>
 						<div id="dotext7">
 							<p><strong>Website Maintenance</strong></p>
-								<p>I offer optional monthly or quarterly maintenance packages to my customers. With my website maintenance packages, you will never have to worry that your website will be out of date or stop working because of changing technologies. If you have any issues or problems with your website I am only a phone call or email away, and will be there with technical support promptly.</p>
+								<p>I offer optional monthly or quarterly maintenance packages to my customers. With my website maintenance packages you will never have to worry that your website will be out of date or stop working because of changing technologies. If you have any issues or problems with your website I am only a phone call or email away, and will be there with technical support promptly. If a website that I built or worked on crashes I will be there to fix it right away. I've got your back!</p>
 						</div>
 					</div>
 				</div><!--inner-dolist-->
@@ -207,16 +207,22 @@ get_header(); ?>
 						</div>
 
 						<div class="special-all">
+
 							<div id="special1">
+								<h5>Promotional Graphics</h5>
+									<p>Does your business have an upcoming sale, promotion or ad campaign? Don't want to spend countless hours designing graphics for all your social media, website & print ads? I offer custom graphic design packages for just those occasions! I'll create banner images for your website and social media accounts. I'll also design a series of promotional graphics that can easily be schedualed into your social media platforms of choice. Only want a couple of simple ads? No job is too small! Need a large series for many months worth of promotions? I can do that too! Ask me about my promotional graphic packages.</p>
+								</div>
+
+							<div id="special2">
 								<h5>Responsive Design Transitions</h5>
 									<p>Most websites these days are responsive and for good reason. As more people switch to using mobile devices such as cell phones and tablets to make their online purchases, it’s more important than ever to have an excellent mobile-friendly website. Nothing loses potential customers faster than a poorly designed website that glitches or is hard to read on a mobile device. If your website is static or needs responsive design updating I can give it a thorough responsive overhaul to keep it current and competitive.<p>
 								</div>
 
-								<div id="special2">
+								<div id="special3">
 									<h5>Site Transfers</h5>
 										<p>Many small businesses and individuals such as bloggers initially start out with a cheap, pre-made website such as Blogger, Weebly, Wix, etc. Often they quickly outgrow these websites and get tired of their built-in limitations. When a customer wishes to switch to a more flexible, custom, and better-designed platform such as WordPress.org I’m here to offer site transfer services. The transfer of domain names, hosting platforms, and migration of content can take a few weeks and can be incredibly complicated for inexperienced users. I am here when a customer needs website transfer help, and will take care of the entire process easily and affordably. </p>
 								</div>
-							</div>
+							</div><!--special-all-->
 					</div>
 
 						<div class="projects">
