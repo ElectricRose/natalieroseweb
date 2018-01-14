a2b16f97be1aa287855def886c047dee0007d33f
Razorcake <sp00kybug@yahoo.ca>
2018-01-13 18:40:15 -0800

    adding changes to master

--- wp-content/themes/fluida-child/about.php
+++ wp-content/themes/fluida-child/about.php
@@ -30,32 +30,35 @@
       </div><!--about-intro-box-->
 
         <div id="about-img">
-          <img src="/wp-content/uploads/2017/06/natalierose-img.jpg" alt="Photo of Natalie Rose">
+          <img src="http://natalierose.net/wp-content/uploads/2018/01/natalieRoseCircle1-450.png" alt="Photo of Natalie Rose">
+        </div>
+
+        <div id="about-work-with-me">
+          <div class="basicBox">
+            <a href="http://natalierose.net/contact/">
+              Let's Chat!
+              <svg width="130" height="65" viewBox="0 0 130 65" xmlns="http://www.w3.org/2000/svg">
+                <rect x='0' y='0' fill='none' width='130' height='65'/>
+              </svg></a>
+          </div>
+          <p>I'm available to answer all your questions. <br> Schedule a FREE 30 min consultation now!</p>
         </div>
 
         <div class="about-desc">
-          <div id="work-img1">
-            <img src="/wp-content/uploads/2017/06/green-computer1.jpg" alt="computer desk with coffee cup and forest screensaver">
-        </div>
-
         <div class="about-text1">
           <h4>My Mission</h4>
-          <p>My mission is to provide affordable, accessible, and beautifully functional web development services to small businesses and individuals. I am an independent freelance service provider that offers a personal experience by working closely on-one-one to create a custom website tailored to your specific needs and budget.</p><br>
+          <p>My mission is to provide affordable, accessible, and beautifully functional web solutions to small businesses and individuals. I am an independent freelance service provider that offers a personal experience by working closely on-one-one to create a custom website tailored to your specific needs and budget.</p><br>
 
           <h4>Have you struggled with website creation in the past?</h4>
-          <h5>Building a website doesn't have to be hard!</h5><br>
-          <p>I can make you a website quickly and easily!</p>
-          <p>While there are many types of do-it-yourself website builders available, many of them have limited design capabilities that can leave the website looking bland and similar to all the others.</p>
-          <p> Many drag and drop build-it-yourself type websites have glitches, errors, and functionality issues that can lose a business customers and money. The free version of WordPress.com is built for bloggers, not small business owners. Many small businesses make the mistake of starting out on these types of services, but quickly realize that they have to pay large amounts to customize it to their needs, and can have severely limited options for monetizing their website. Often the add-ons can end up costing much more than a customized website on WordPress.org.</p>
-          <p>If you find WordPress customization and PHP hard to understand you aren't alone. But...</p><br>
-          <h6>Don't worry!</h6>
-
+          <h5>Building a website doesn't have to be hard!</h5>
+          <p>When you need custom designs, features, and functionalities</p>
             <div id="can-help">
               <div class="container borderXwidth">
-                <p><a href="http://natalierose.net/work/">I can help!</a></p><br>
+                <p><a href="http://natalierose.net/work/">I'm here to help!</a></p><br>
               </div>
             </div>
-        </div>
+        </div><!--about-text1-->
+      </div><!--about-desc-->
 
       <div class="about-container">
         <div class="about-text2">
@@ -65,21 +68,13 @@
           <p>This common and frustrating error can cost you a lot of business. Don't miss out on opportunities because of simple website mistakes or oversights.</p>
           <p>I can make quick and easy changes to your website for a small fee. Contact me to request a free quote. <br>No job is too small!</p>
 
-        </div>
-            <div id="about-work-with-me">
-              <div id="teacup">
-                <img src="/wp-content/uploads/2017/06/teacup1.png" alt="Cup of tea">
-              </div>
-              <p>Let's chat!<br> I'm available to meet in-person or online.</p><br>
-              <div class="basicBox">
-                <a href="http://natalierose.net/contact/">
-                  Contact Me!
-                  <svg width="130" height="65" viewBox="0 0 130 65" xmlns="http://www.w3.org/2000/svg">
-                    <rect x='0' y='0' fill='none' width='130' height='65'/>
-                  </svg></a>
-              </div>
-            </div>
+        </div><!--about-text2-->
+
+          <div class="shop-pic">
+            <img src="http://natalierose.net/wp-content/uploads/2018/01/rest-plate700.jpg" alt="Fancy food on table at restaurant">
           </div>
+
+      </div><!--about-container-->
 
         <div class="about-container2">
           <div class="about-text3">
@@ -91,12 +86,9 @@
              <p>I love to support small businesses, individuals, and start-ups to achieve their own goals by providing them with an instrumental tool to success. I understand the financial pressures of self-employment and small business. That's why I keep my prices lower than average. I want to serve my local and wider communities well, while also being able to feed my family.</p><br>
             <h5>I love this beautiful planet.</h5>
             <p>I strive to be as sustainable and environmentally friendly as possible. When I’m not online or neck deep in code, I spend as much time as possible exploring the wild nooks and spectacular vistas nature has to offer.</p><br>
-            <p>On my off time, you can find me wandering the forests, climbing mountains, swimming in the ocean, and tubing down rivers. <br>If you want to see more of this side of me check out my lifestyle blog filled with my adventures, my love of the outdoors, and self-sustainability tips like working with essential oils, gardening, cooking, and DIY projects.</p>
-            <p>Give me some love on social media or browse my pics on Instagram!</p><br>
+            <p>On my off time, you can find me wandering the forests, climbing mountains, swimming in the ocean, and tubing down rivers. <br>If you want to see more of this side of me check out my lifestyle blog filled with my adventures, my love of the outdoors, and self-sustainability tips like working with essential oils, gardening, cooking, and DIY projects.</p><br>
             <h5>Want to see more of me?</h5>
-            <div class="container borderXwidth">
-            <h5><a href="http://www.natalieroseblog.com">Check out my blog!</h5></p>
-            </div>
+            <p>Give me some love on social media or browse my pics on Instagram!</p><br>
 
             <div class="icons center-block">
           		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
