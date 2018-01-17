09fa0332d4eb7594e63c098ff0f0fce02a24917a
Razorcake <sp00kybug@yahoo.ca>
2018-01-17 02:43:45 -0800

    changes

--- wp-content/themes/fluida-child/front-page.php
+++ wp-content/themes/fluida-child/front-page.php
@@ -54,15 +54,11 @@ function fluida_lp_layoutclass () {
   <div class="front-work">
 		<div class="front-rainbow">
 		<div class="front-work-section">
-      <h3>Do you need a website?</h3>
-      <p><span id="problems">Are you a small business or individual with a product, service, or idea to share?<br><br>
-			Are you looking for something elegant, dynamic, and easy to use?<br><br>
-			Are you struggling to create a website and stay within your budget?</p></span><br>
-			<p><span id="front-solutions">I have solutions!</span></p>
-			<p><span id="i-build">I build websites for small businesses, blogs, and start-ups.</span></p><br>
-			<p><strong>You work hard, and you need your website to work hard for you!</strong></p>
-			<p>Your business is as unique as you are. </p>
-			<p>I work closely with my clients to create a truly one-of-a-kind website tailored to you and your business.</p>
+      <h3>Future growth by design</h3>
+      <p><span id="">Let me guess, you are here because you need some help to grow your business.</span><br><span id="">You have the passion and the skills.<br> You have the drive and the ambition.<br> You have amazing gifts that you want to share with the people who need them.<br> But what you lack is the steady stream of customers begging for your help.</span><br>Am I right?<br> Maybe you have tried on your own for a while now, or maybe you are just getting started. You are overwhelmed and frustrated and your website or lack-there-of isn’t working for you.<br> You need a change, something different, and just building a website isn’t going to solve all of your problems.<br> Sure you can throw your money at the problem, and hope it works out.<br><span id=""> Or you can invest in someone who wants to see you succeed.</span><br>You need a plan, an amazing website, and a brand that captures your customer’s imagination. You need someone who can see who you are and what you have to offer the world. Someone who can unearth the essence of your business and deliver it in a meaningful and captivating way.
+			<br> You need Natalie Rose.
+			<br>When you are ready to dig deep, invest in yourself and your business, and take it to the next level I’m here to help.<br>
+			I can help you by designing and providing you with a signature style and cohesive brand that speaks to the true essence of your business. We can work together to develop a strategy to promote your business and capture leads through branded graphics and advertising. And I can build you a website that functions flawlessly and helps to turn those leads into paying customers.<br> The future is yours. Are you ready to grab it?</p>
         <div id="work-more">
 					<div class="frontworkBox">
 						<a href="http://natalierose.net/work/">Learn More
@@ -77,10 +73,8 @@ function fluida_lp_layoutclass () {
 
   <div class="front-about">
       <div class="front-about-text">
-        <h3>Reach a wider audience online with a custom website.</h3>
-          <p>If you need a website that is powerful, easy to use, and fully responsive that won't break the bank consider Natalie Rose for your web design & development needs.<br>
-					Designing and building websites is my passion.
-					I craft custom websites from scratch so you can focus on what you do best. Grow your business online with a custom website and experience the unlimited possibilities the online business world has to offer. Stand out in your local community or reach potential customers from all over the world. From Ecommerce to brick and mortar businesses, I've got web solutions for you!<p>
+        <h3>Reach a wider audience online</h3>
+          <p>I build solid brands & craft engaging websites.<br>I design & develop for the freedom junkies, the holistic healers, health & wellness providers, and the creative entrepreneurs. I also love to work with businesses and organizations focused on nature, outdoor adventure, and eco-tourism.<br>If you want to grow your business online and attract more customers, I can help you to stand out in your local community or reach potential customers from all over the world. From Ecommerce to brick and mortar businesses, I've got web solutions for you! You don't have to struggle alone, I'm here to take this journey with you.<p>
 			</div>
 			<div id="front-about-img">
 	      <img src="http://natalierose.net/wp-content/uploads/2018/01/profile-circle3.png">
@@ -96,7 +90,7 @@ function fluida_lp_layoutclass () {
   	</div><!--front-about-->
 
 	<div class="front-contact">
-		<h5>Want to work with me?</h5>
+		<h5>Interested? Want to know more?</h5>
 		<p>I'm available:<br>
 			Mon - Fri<br>
 			9am - 5pm<br>
@@ -105,7 +99,7 @@ function fluida_lp_layoutclass () {
 		<div id="work-with-me">
 			<div class="basicBox">
 				<a href="http://natalierose.net/contact/">
-					Contact Me
+					Let's Chat!
 				  <svg width="130" height="65" viewBox="0 0 130 65" xmlns="http://www.w3.org/2000/svg">
 				    <rect x='0' y='0' fill='none' width='130' height='65'/>
 				  </svg></a>
